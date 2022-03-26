c4728b8b8a45332e642cdd4e1e27a2db478ed78b
Tyler Long <tyler4long@gmail.com>
2022-03-26 09:50:07 -0700

    Refactor code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
-const path = require('path');
+const { join } = require('path');
 
 module.exports = {
-  "root": true,
-  "parserOptions": {
-    "project": path.join(__dirname, "tsconfig.json").replace('app.asar', 'app.asar.unpacked')
+  root: true,
+  parserOptions: {
+    project: join(__dirname, 'tsconfig.json').replace('app.asar', 'app.asar.unpacked'),
   },
-  "extends": [
-    "eslint-config-airbnb-base",
-    "eslint-config-airbnb-typescript/base"
-  ]
-}
+  extends: [
+    'eslint-config-airbnb-base',
+    'eslint-config-airbnb-typescript/base',
+  ],
+};

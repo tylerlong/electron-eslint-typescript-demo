47efda1d0e9ac62d3de42fba76368cfa3126eb7d
Tyler Long <tyler4long@gmail.com>
2022-03-25 18:14:58 -0700

    Hello Electron

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+const {app, BrowserWindow} = require('electron')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  })
+  mainWindow.loadFile('src/index.html')
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})

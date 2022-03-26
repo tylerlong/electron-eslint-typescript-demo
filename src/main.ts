4196aa2fb9405c10e0e2dd53e86f7731f1052875
Tyler Long <tyler4long@gmail.com>
2022-03-25 18:22:32 -0700

    Hello ESLint

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron');
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-  })
-  mainWindow.loadFile('src/index.html')
+  });
+  mainWindow.loadFile('src/index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
